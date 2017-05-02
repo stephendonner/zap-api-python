2a2fbc710e80b67ebf16de338d1d7d94d51151f0
Stephen Donner <stephen.donner@gmail.com>
2017-05-02 15:34:38 -0700

    Change target to localhost, importURL to swagger.io

--- src/examples/basic-spider-scan.py
+++ src/examples/basic-spider-scan.py
@@ -13,8 +13,8 @@ zap = ZAPv2(apikey=apikey)
 # Use the line below if ZAP is not listening on port 8080, for example, if listening on port 8090
 # zap = ZAPv2(apikey=apikey, proxies={'http': 'http://127.0.0.1:8090', 'https': 'http://127.0.0.1:8090'})
 
-zap._request(zap.base + 'openapi/action/importUrl/',{'url':'https://kinto.dev.mozaws.net/v1/__api__'})
-target = 'https://kinto.dev.mozaws.net/v1/'
+zap._request(zap.base + 'openapi/action/importUrl/',{'url':'http://petstore.swagger.io/v2/swagger.json'})
+target = 'http://127.0.0.1'
 
 # Proxy a request to the target so that ZAP has something to deal with
 print 'Accessing target %s' % target
